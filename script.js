d7e81a735aa0f37ba0eb58c281ecbd4cca1e994c
kelenam <sp4cet04st@gmail.com>
2020-02-26 07:40:12 -0800

    Refactoring: using dataset instead of attributes

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 (function init() {
+  const keys = document.querySelectorAll(".key");
+  keys.forEach(key => {
+    key.addEventListener("click", playSound);
+    key.addEventListener("transitionend", removeTransition);
+  });
 
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key => {
-        key.addEventListener('click', playSound);
-        key.addEventListener('transitionend', removeTransition);
-    });
-
-    console.log("Image credit: Gabriel Barletta via Unsplash.com\nProject credit: Wesbos's JS30 series")
+  console.log(
+    `Image credit: Gabriel Barletta via Unsplash.com\nProject credit: Wesbos's JS30 series`
+  );
 })();
 
-function playSound(e) { 
-    
-    let keyCode = e.keyCode;
+function playSound(e) {
+  let keyCode = e.keyCode;
 
-    if (e.type === "click") {
-        keyCode = this.attributes[0].value;
-    }
+  if (e.type === "click") {
+    keyCode = this.dataset.key;
+  }
 
-    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${keyCode}"]` );
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
 
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 
-    key.classList.add('playing'); 
+  key.classList.add("playing");
 }
 
 function removeTransition(e) {
-    this.classList.remove('playing');
+  this.classList.remove("playing");
 }
 
-window.addEventListener('keydown', playSound);
+window.addEventListener("keydown", playSound);
